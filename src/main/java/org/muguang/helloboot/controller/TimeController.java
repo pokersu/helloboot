59ab5c131e672c705540ac34af16f55ad1e67a25
pokersu <pokersugo@gmail.com>
2023-03-12 19:18:36 +0800

    update time controller

--- src/main/java/org/muguang/helloboot/controller/TimeController.java
+++ src/main/java/org/muguang/helloboot/controller/TimeController.java
@@ -2,6 +2,8 @@ package org.muguang.helloboot.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,13 @@ public class TimeController {
 
     @GetMapping
     public String currentTime() {
-
         LocalDateTime curTime = LocalDateTime.now();
+        var list = List.of("test", "nvim", "from remote machine");
+        for (String str : list) {
+            System.out.println(str);
+        }
+        var superlong = list.stream().filter(s->s.length()>10).collect(Collectors.toList());
+        superlong.stream().forEach(System.out::println);
         return curTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
     
